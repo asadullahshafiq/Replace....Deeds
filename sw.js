e1723aaefbd85905b9ac1393c9be2758dbc5f040
Mr.Asad <tigerbro1515@gmail.com>
2026-04-22 15:50:09 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,27 +1,40 @@
-const CACHE_NAME = 'replace-deeds-v6';
-const ASSETS = ['./index.html', './manifest.json', './icon-192.svg', './icon-512.svg'];
+const CACHE_NAME = 'replace-deeds-v3';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.svg',
+  './icon-512.svg'
+];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS).catch(err => console.log('Cache add error:', err));
+    })
+  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
   self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('./index.html'))));
-});
-
-// Background notifications
-self.addEventListener('message', async (event) => {
-  if (event.data?.type === 'SHOW_NOTIFICATION') {
-    await self.registration.showNotification(event.data.title, {
-      body: event.data.body,
-      icon: '/icon-192.svg',
-      requireInteraction: true
-    });
-  }
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      if (cached) return cached;
+      return fetch(e.request).then(res => {
+        if (!res || res.status !== 200 || res.type === 'opaque') return res;
+        const clone = res.clone();
+        caches.open(CACHE_NAME).then(c => c.put(e.request, clone));
+        return res;
+      }).catch(() => caches.match('./index.html'));
+    })
+  );
 });
